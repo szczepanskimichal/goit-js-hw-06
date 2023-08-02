4c87bc6502a3dbdc13412d22633fdb7bef0e683b
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-08-02 09:06:10 +0200

    w

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,9 @@
-// Pobieramy elementy DOM
 const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
-// Funkcja do aktualizacji zawartości spana na podstawie wprowadzonego tekstu
 function updateNameOutput() {
-  const name = nameInput.value.trim(); // Usuwamy zbędne spacje z początku i końca wprowadzonego tekstu
-  nameOutput.textContent = name || "Anonymous"; // Jeśli name nie jest puste, aktualizujemy zawartość spana, w przeciwnym razie wyświetlamy "Anonymous"
+  const name = nameInput.value.trim();
+  nameOutput.textContent = name || "Anonymous";
 }
 
-// Dodajemy listener na zdarzenie input na polu input#name-input
 nameInput.addEventListener("input", updateNameOutput);
